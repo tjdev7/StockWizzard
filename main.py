14cc4225bc2c00dd24502a0055a4e9eb6050e71a
Mario Jimenez <Trimmask@protonmail.com>
2023-10-04 16:39:23 -0500

    StockWizzard/002/Stock_Data_and_Front_End_Update
    - Added new documentation for the data science app
    - Added new screenshots and pictures for the repo
    - Developed the initial layout and user elements for the app. The app now provides historical 1 year data for 7 preselected companies in the NASDAQ index marketplace

--- main.py
+++ main.py
@@ -1,19 +1,71 @@
-import yfinance as finance
 import streamlit as st
-
-print("StockWizzard")
-
-print("A Stock ticker data science app showing realtime stock data for US based companies")
-
-# Sections of the app
+import yfinance as finance
 
 
-## __init_ section
+def stock_ticker(name):
+	company = finance.Ticker(name) 
+	return company
 
-## get stock tickers
+st.title("StockWizzard")
 
-## get financial periods
+company1 = stock_ticker("TSLA")
+company2 = stock_ticker("AAPL")
+company3 = stock_ticker("AMZN")
+company4 = stock_ticker("V")
+company5 = stock_ticker("DOW")
+company6 = stock_ticker("BRK-A")
+company7 = stock_ticker("MA")
 
-## generate graphs
 
-## finalize the application
+
+
+tesla = finance.download("TSLA", start="2022-10-01", end="2023-10-01")
+apple = finance.download("AAPL", start="2022-10-01", end="2023-10-01")
+amazon = finance.download("AMZN", start="2022-10-01", end="2023-01-01")
+visa = finance.download("V", start="2022-10-01", end="2023-01-01")
+dow = finance.download("dow", start="2022-10-01", end="2023-01-01")
+brka = finance.download("BRK-A", start="2022-10-01", end="2023-01-01")
+mstrc = finance.download("MA", start="2022-10-01", end="2023-01-01")
+
+
+
+data1 = company1.history(period="1y")
+data2 = company2.history(period="1y")
+data3 = company3.history(period="1y")
+data4 = company4.history(period="1y")
+data5 = company5.history(period="1y")
+data6 = company6.history(period="1y")
+data7 = company7.history(period="1y")
+
+
+
+
+st.write("""### Tesla Inc. """)
+st.write(tesla)
+st.line_chart(data1.values)
+
+st.write("""### Apple Inc. """)
+st.write(apple)
+st.line_chart(data2.values)
+
+
+st.write("""### Amazon.com Inc. """)
+st.write(amazon)
+st.line_chart(data3.values)
+
+
+st.write("""### Visa""")
+st.write(visa)
+st.line_chart(data4.values)
+
+st.write("""### DOW Inc. """)
+st.write(dow)
+st.line_chart(data5.values)
+
+st.write("""### Berkshire Hathaway Inc.  """)
+st.write(brka)
+st.line_chart(data6.values)
+
+st.write("""### Mastercard  """)
+st.write(mstrc)
+st.line_chart(data7.values)
